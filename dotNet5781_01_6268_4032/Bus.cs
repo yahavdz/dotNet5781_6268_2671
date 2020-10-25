4057b920fbbae5bd97cbcadc0279a815fcbd034d
משה קופרמן <moshecop@gmail.com>
2020-10-25 21:53:40 +0200

    first try

--- dotNet5781_01_6268_4032/Bus.cs
+++ dotNet5781_01_6268_4032/Bus.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace dotNet5781_01_6268_4032
 {
-    class Bus
+    class Buss
     {
-        private int busID { set; get; }
-        private int km { set; get; }
-        private int kmHandler { set; get; }
-        private int kmFule { set; get; }
-        private int date { set; get; }
-        
+        public int bussID { set; get; }
+        public int km { set; get; }
+        public int kmHandler { set; get; }
+        public int kmFule { set; get; }
+        public DateTime date { set; get; }
+        private static int maxKmHandler = 20000;
+        private static int maxKmFule = 1200;
     }
 }
--- dotNet5781_01_6268_4032/Main.cs
+++ dotNet5781_01_6268_4032/Main.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,31 @@ namespace dotNet5781_01_6268_4032
     {
         static void Main(string[] args)
         {
-            List<Bus> busList = new List<Bus>();
+            int readBussID, readKm, readKmHandler, readKmFule, readDate;
+            List<Buss> bussList = new List<Buss>();
+            userChoiceEnum choice;
+            Console.WriteLine( "Please enter your choise/n 1 for insert, 2 for choosing a buss");
+            choice = (userChoiceEnum)Console.Read();
+            switch (choice)
+            {
+                case userChoiceEnum.insertBuss:
+                    bussList.Add(new Buss
+                    {
+                        bussID = readBussID,
+                        date = readDate
+                    }) ;
+                    break;
+                case userChoiceEnum.chooseBuss:
+                    break;
+                default:
+                    break;
+            }
         }
     }
+    public enum userChoiceEnum
+    {
+        insertBuss=1, 
+        chooseBuss=2,
+    }
+  
 }
