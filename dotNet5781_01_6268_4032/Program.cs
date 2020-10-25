44f1aaae01081fba549d2ca72e8b363b22a296f3
yahav davidzad <davidzad@g.jct.ac.il>
2020-10-25 20:37:07 +0200

    new solution 01

---
+++ dotNet5781_01_6268_4032/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_6268_4032
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            0
+        }
+    }
+}
