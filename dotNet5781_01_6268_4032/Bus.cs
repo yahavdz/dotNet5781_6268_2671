5b39a8704023917e762af28227ee4f0faf3d8e83
yahav davidzad <davidzad@g.jct.ac.il>
2020-10-27 23:40:34 +0200

    finish ex1

--- dotNet5781_01_6268_4032/Bus.cs
+++ dotNet5781_01_6268_4032/Bus.cs
@@ -9,30 +9,26 @@ namespace dotNet5781_01_6268_4032
 {
     class Bus
     {
-        public string bussID { set; get; }
+        public static int maxMileageTreatment = 20000;
+        public static int maxmileageFuel = 1200;
+
+        public string busID { set; get; }
         public int mileage { set; get; }
         public int mileageTreatment { set; get; }
-        public int mileageFule { set; get; }
-        public DateTime treatmentDate { set; get; }
-        private static int maxMileageTreatment = 20000;
-        private static int maxMileageFule = 1200;
-        public static int maxKmHandler = 20000;
-        public static int maxKmFule = 1200;
-        public string busID { set; get; }
-        public int km { set; get; }
-        public int treatmentKm { set; get; }
-        public int kmFule { set; get; }
+        public int mileageFuel { set; get; }
         public DateTime treatmentDate { set; get; }
 
         public void treatment()
         {
-            treatmentKm = km;
+            mileageTreatment = mileage;
             treatmentDate = DateTime.Now;
         }
         public void refueling()
         {
-            kmFule = maxKmFule;
+            mileageFuel = mileage;
         }
+
+        
 
     }
 }
--- dotNet5781_01_6268_4032/Main.cs
+++ dotNet5781_01_6268_4032/Main.cs
@@ -9,138 +9,170 @@ namespace dotNet5781_01_6268_4032
 {
     class Program
     {
-        static void Main(string[] args)
+        static Bus insertBus()
         {
-            int readMileage, readMileageTreatment, readMileageFule; // these variables are for the user to insert and then for the program to use.
+            int readMileage, readMileageTreatment, readmileageFuel; // these variables are for the user to insert and then for the program to use.
 
-            string readBussID;
-            bool successReadingBussID = true; // to use for the "do-while" loop
+            string readbusID;
+            bool successReadingbusID = true; // to use for the "do-while" loop
 
             DateTime readTreatmentDate;
+            Console.WriteLine("Please enter activity start date");
 
-            List<Buss> bussList = new List<Buss>();
+            readTreatmentDate = DateTime.Parse(Console.ReadLine()); // conversion from string to DateTime
 
+            Console.WriteLine("Please enter bus ID");
 
+            readbusID = Console.ReadLine(); // receiving the bus ID from the user
 
-            userChoiceEnum choice; // the definition is under the main
-            choice = (userChoiceEnum)Console.Read(); // receiving a choise from the user for the enum
-            Console.WriteLine( "Please enter your choise/n 1 for insert, 2 for choosing a buss");
-            
-            switch (choice)
+            string[] splitReadbusID = readbusID.Split('-'); // spliting the receiving string with '-' to check the format
+
+            do
             {
-                case userChoiceEnum.insertBuss: // case 1
-
-                    Console.WriteLine("Please enter activity start date/n");
-
-                    readTreatmentDate = DateTime.Parse( Console.ReadLine()); // conversion from string to DateTime
-                    
-                    Console.WriteLine("Please enter buss ID/n");
-                    
-                    readBussID = Console.ReadLine(); // receiving the buss ID from the user
-                    
-                    string[] splitReadBussID = readBussID.Split('-'); // spliting the receiving string with '-' to check the format
-                    
-                    do
+                successReadingbusID = true;
+                if (readTreatmentDate.Year < 2018) // format 2-3-2
+                {
+                    if (!(splitReadbusID[0].Length == 2 && splitReadbusID[1].Length == 3 && splitReadbusID[2].Length == 2)) // the format should be 2-3-2
                     {
-                        if (readTreatmentDate.Year < 2018) // format 2-3-2
-                        {
-                            if (!(splitReadBussID[0].Length == 2 && splitReadBussID[1].Length == 3 && splitReadBussID[2].Length == 2)) // the format should be 2-3-2
-                            {
-                                Console.WriteLine("Format is incorrect, please try again/n");
-                                successReadingBussID = false;
-                                readBussID = Console.ReadLine(); // receiving the buss ID from the user
-                                splitReadBussID = readBussID.Split('-'); // spliting the receiving string with '-' to check the format
-                            }
-                        }
-                        if (readTreatmentDate.Year >= 2018) // format 3-2-3
-                        {
-                            if (!(splitReadBussID[0].Length == 3 && splitReadBussID[1].Length == 2 && splitReadBussID[2].Length == 3)) // the format should be 3-2-3
-                            {
-                                Console.WriteLine("Format is incorrect, please try again/n");
-                                successReadingBussID = false; 
-                                readBussID = Console.ReadLine(); // receiving the buss ID from the user
-                                splitReadBussID = readBussID.Split('-'); // spliting the receiving string with '-' to check the format
-                            }
-                        }
-                        
-                    } while (successReadingBussID==false); // if successReadingBussID==true it continues on
-
-                    Console.WriteLine("Please enter current mileage/n");
-                    readMileage = Console.Read();
-
-                    Console.WriteLine("Please enter last mileage treatment/n");
-                    readMileageTreatment = Console.Read();
-                    
-                    Console.WriteLine("Please enter since last refuling mileage/n");
-                    readMileageFule = Console.Read();
-                    
-                    bussList.Add(new Buss // copying the reading variables to the original variables in the class Buss and putting them in a new Buss item
-                    {
-                        busID = readBusID,
-                        date = readDate
-                    });
-                    break;
-
-                case userChoiceEnum.chooseBus:
-                    Console.WriteLine("please ente the bus id: ");
-                    string busid = Console.ReadLine();
-                    Random r = new Random(DateTime.Now.Millisecond);
-                    int randomKm = r.Next(Bus.maxKmHandler);
-                    Bus currentBus1 = busList.Find(b => b.busID.Equals(busid));
-                    if (currentBus1 == null)
-                    {
-                        Console.WriteLine("Error: you entered invalid id, bus not found");
-                        return;
+                        Console.WriteLine("Format is incorrect, please try again\n");
+                        successReadingbusID = false;
+                        readbusID = Console.ReadLine(); // receiving the bus ID from the user
+                        splitReadbusID = readbusID.Split('-'); // spliting the receiving string with '-' to check the format
                     }
+                }
+                if (readTreatmentDate.Year >= 2018) // format 3-2-3
+                {
+                    if (!(splitReadbusID[0].Length == 3 && splitReadbusID[1].Length == 2 && splitReadbusID[2].Length == 3)) // the format should be 3-2-3
+                    {
+                        Console.WriteLine("Format is incorrect, please try again\n");
+                        successReadingbusID = false;
+                        readbusID = Console.ReadLine(); // receiving the bus ID from the user
+                        splitReadbusID = readbusID.Split('-'); // spliting the receiving string with '-' to check the format
+                    }
+                }
 
-                    if (currentBus1.km + randomKm >= Bus.maxKmHandler)
-                    {
-                        Console.WriteLine("Error: the bus cant take the ride, too many km");
-                        return;
-                    }
-                    else if ((currentBus1.kmFule - randomKm) < 0)
-                    {
-                        Console.WriteLine("Error: the bus cant take the ride, the fule is too low");
-                        return;
-                    }
-                    else
-                    {
-                        currentBus1.km += randomKm;
-                        currentBus1.kmFule -= randomKm;
-                    }
-                    break;
+            } while (successReadingbusID == false); // if successReadingbusID==true it continues on
 
-                case userChoiceEnum.treatmentOrRefueling:
-                    Console.WriteLine("please enter bus id: ");
-                    string id = Console.ReadLine();
-                    Bus currentBus = busList.Find(b => b.busID.Equals(id));
-                    if (currentBus == null)
-                    {
-                        Console.WriteLine("bus not found");
-                        return;
-                    }
-                    Console.WriteLine("Please enter your choise\n 1 for treatment, 2 for refuling");
-                    string input = Console.ReadLine();
-                    if (input == "1")
-                    {
-                        currentBus.treatment();
-                    }
-                    else
-                    {
-                        currentBus.refueling();
-                    }
-                    break;
+            Console.WriteLine("Please enter current mileage");
+            Int32.TryParse(Console.ReadLine(), out readMileage);
 
-                case userChoiceEnum.printKm:
-                    foreach (Bus bus in busList)
-                    {
-                        Console.WriteLine(bus.busID + "the current km is: " + bus.km + ". the km from the last treatment until now is: " + (bus.km - bus.treatmentKm));
-                    }
-                    break;
+            Console.WriteLine("Please enter the mileage from the last treatment");
+            Int32.TryParse(Console.ReadLine(), out readMileageTreatment);
 
-                default:
-                    break;
+            Console.WriteLine("Please enter the mileage since the last refueling");
+            Int32.TryParse(Console.ReadLine(), out readmileageFuel);
+
+            Bus bus = new Bus // copying the reading variables to the original variables in the class bus and putting them in a new bus item
+            {
+                busID = readbusID,
+                treatmentDate = readTreatmentDate,
+                mileage = readMileage,
+                mileageTreatment = readMileageTreatment,
+                mileageFuel = readmileageFuel
+            };
+            Console.WriteLine();
+            return bus;
+            
+        }
+        static void chooseNewBus(List<Bus> busList)
+        {
+            Console.WriteLine("please enter the bus id: ");
+            string busid = Console.ReadLine();
+            Random r = new Random(DateTime.Now.Millisecond);
+            int randomKm = r.Next(Bus.maxMileageTreatment);
+            Console.WriteLine("the number of the miles is: " + randomKm);
+            Bus currentBus1 = busList.Find(b => b.busID.Equals(busid));
+            if (currentBus1 == null)
+            {
+                Console.WriteLine("Error: you entered invalid id, bus not found\n");
+                return;
             }
+
+            if ((currentBus1.mileage-currentBus1.mileageTreatment + randomKm) >= Bus.maxMileageTreatment)
+            {
+                Console.WriteLine("Error: the bus cant take the ride, too many km\n");
+                return;
+            }
+            if ((currentBus1.mileageFuel - randomKm) < 0)
+            {
+
+                Console.WriteLine("Error: the bus cant take the ride, the fule is too low\n");
+                return;
+            }
+            
+            
+                Console.WriteLine("Sucsess! the bus can take the drive\n");
+                currentBus1.mileage += randomKm;
+                currentBus1.mileageFuel -= randomKm;
+            
+            return;
+        }
+        static void treatmentOrRefuelingForBus(List<Bus> busList)
+        {
+            Console.WriteLine("please enter bus id: ");
+            string id = Console.ReadLine();
+            Bus currentBus = busList.Find(b => b.busID.Equals(id));
+            if (currentBus == null)
+            {
+                Console.WriteLine("bus not found\n");
+                return;
+            }
+            Console.WriteLine("Please enter your choise\n 1: for treatment\n 2: for refuling");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                currentBus.treatment();
+            }
+            else
+            {
+                currentBus.refueling();
+            }
+        }
+        static void printList(List<Bus> busList)
+        {
+            foreach (Bus bus in busList)
+            {
+                Console.WriteLine(bus.busID + " \n the current km is: " + bus.mileage + "\n the km from the last treatment until now is: " + (bus.mileage - bus.mileageTreatment)+ "\n the last refueling: "+bus.mileageFuel);
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            List<Bus> busList = new List<Bus>();
+            Console.WriteLine("Please enter your choise:\n 1: for insert\n 2: for choosing a bus\n 3: for treatment or refueling\n 4: for printing all the bus's mileage from the last treatment\n 5: exit\n");
+            userChoiceEnum choice; // the definition is under the main
+            Enum.TryParse(Console.ReadLine(), true, out choice); // convert the input to the enum and enter it into choice
+
+            while (choice != userChoiceEnum.exit)
+            {
+                switch (choice)
+                {
+                    case userChoiceEnum.insertBus: // case 1
+                        Bus newBus = insertBus();
+                        busList.Add(newBus);
+                        break;
+
+                    case userChoiceEnum.chooseBus:
+                        chooseNewBus(busList);
+                        break;
+
+                    case userChoiceEnum.treatmentOrRefueling:
+                        treatmentOrRefuelingForBus(busList);
+                        break;
+
+                    case userChoiceEnum.printKm:
+                        printList(busList);
+                        break;
+
+                    default:
+                        break;
+                     
+                }
+                Console.WriteLine("Please enter your choise:\n 1: for insert\n 2: for choosing a bus\n 3: for treatment or refueling\n 4: for printing all the bus's mileage from the last treatment\n 5: exit\n");
+                Enum.TryParse(Console.ReadLine(), true, out choice); // convert the input to the enum and enter it into choice
+            }
+                
         }
     }
     public enum userChoiceEnum
@@ -148,8 +180,12 @@ namespace dotNet5781_01_6268_4032
         insertBus = 1,
         chooseBus = 2,
         treatmentOrRefueling = 3,
-        printKm = 4
+        printKm = 4,
+        exit = 5
 
     }
 
+    
 }
+
+
