ec59645517f674c93fad5b9e3b991c2971e98400
משה קופרמן <moshecop@gmail.com>
2020-11-06 12:26:38 +0200

    creating new class - BusCollection and creating methods

---
+++ dotNet5781_02_6268_4032/BusCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_6268_4032
+{
+    class BusCollection
+    {
+        public List<BusLine> BusLines { set; get; }
+        public void addBus(BusLine bus, BusCollection busCollection)
+        {
+
+        }
+        public void deleteBus(int busID)
+        {
+
+        }
+        public List<BusLine> getBusLinesOfStation(BusStation busStation)
+        {
+
+        }
+        public List<BusLine> getBusListByLengthOfRide(BusCollection busCollection)
+        {
+
+        }
+        public void index(int value)
+        {
+
+        }
+    }
+}
